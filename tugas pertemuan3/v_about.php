d373d0882d6635a76cd8853c62c1917581fc866a
agusbahrirahman <101780794+agusbahrirahman@users.noreply.github.com>
2022-04-05 00:03:42 +0700

    Create v_about.php

---
+++ tugas pertemuan3/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Agus Bahri</li>
+        <li>Nama Belakang : Rahman</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Pemuda No.01</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cisauk</li>
+    </lu>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Sepak Bola</li>
+        <li>Bulu Tangkis</li>
+    </ul>
+</section>
